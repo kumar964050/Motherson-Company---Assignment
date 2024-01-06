dbd9c7c7cd03389d3c39fae12f73ec01b2137725
kumar964050 <kumar964050@gmail.com>
2024-01-07 01:45:06 +0530

    3rd commit

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -16,7 +16,9 @@ const Home = () => {
             Short text with details about you, what you do or your professional
             career. You can add more information on the about page.
           </p>
-          <button className={styles.primaryBtn}>Projects</button>
+          <a href="#Projects">
+            <button className={styles.primaryBtn}>Projects</button>
+          </a>
           <button className={styles.secondaryBtn}>Linkedin</button>
         </div>
       </section>
